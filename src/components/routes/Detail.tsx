fc3a2647d8fc98ab2b7d926b16436f0f25f530fd
Nick Breaton <nick@nickbreaton.com>
2026-01-31 15:37:12 -0500

    Add image to details page

--- src/components/routes/Detail.tsx
+++ src/components/routes/Detail.tsx
@@ -1,7 +1,15 @@
+import { Result, useAtomValue } from "@effect-atom/atom-react";
+import { getPhotoUrlAtom } from "../../effect/client/atom";
+import { useParams } from "wouter";
+
 export const Detail = () => {
+  const { id } = useParams();
+  const url = useAtomValue(getPhotoUrlAtom(id!));
+  const src = Result.isSuccess(url) ? url.value : undefined;
   return (
-    <div>
+    <div className="px-5">
       <h1>Detail</h1>
+      <img src={src} />
     </div>
   );
 };
