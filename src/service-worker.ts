d7752c1cb49dcd253543b88cee5751b3118fa636
Nick Breaton <nick@nickbreaton.com>
2026-02-03 22:16:47 -0500

    Ignore cloudflare specific script

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -4,4 +4,5 @@ import { precacheAndRoute } from "workbox-precaching";
 precacheAndRoute(self.__WB_MANIFEST, {
   directoryIndex: "index.html",
   cleanURLs: true,
+  ignoreURLParametersMatching: [/_worker\.js/i],
 });
