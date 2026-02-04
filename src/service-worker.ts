f1c0cc7630428a5c43b9a9d8e346b782a6520407
Nick Breaton <nick@nickbreaton.com>
2026-02-04 08:42:12 -0500

    Fix offline HTML serving via NavigationRoute

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,16 +1,19 @@
 import { CacheableResponsePlugin } from "workbox-cacheable-response";
 import { ExpirationPlugin } from "workbox-expiration";
-import { cleanupOutdatedCaches, precacheAndRoute } from "workbox-precaching";
-import { registerRoute } from "workbox-routing";
+import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from "workbox-precaching";
+import { NavigationRoute, registerRoute } from "workbox-routing";
 import { CacheFirst, StaleWhileRevalidate } from "workbox-strategies";
 
 cleanupOutdatedCaches();
 
 // @ts-ignore
-precacheAndRoute(self.__WB_MANIFEST, {
-  directoryIndex: "index.html",
-  cleanURLs: true,
-});
+precacheAndRoute(self.__WB_MANIFEST);
+
+// Handle navigation requests by serving the precached index.html
+// This enables the app to work offline when navigating to any route
+const handler = createHandlerBoundToURL("/index.html");
+const navigationRoute = new NavigationRoute(handler);
+registerRoute(navigationRoute);
 
 // dprint-ignore
 registerRoute(({ url }) => url.origin === "https://fonts.googleapis.com", new StaleWhileRevalidate({
