90c35ec85df6a10330d141f81cdd59905ce9d38d
Nick Breaton <nick@nickbreaton.com>
2026-02-04 00:03:01 -0500

    Use outfit font, cache via server worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,4 +1,10 @@
-import { precacheAndRoute } from "workbox-precaching";
+import { CacheableResponsePlugin } from "workbox-cacheable-response";
+import { ExpirationPlugin } from "workbox-expiration";
+import { cleanupOutdatedCaches, precacheAndRoute } from "workbox-precaching";
+import { registerRoute } from "workbox-routing";
+import { CacheFirst, StaleWhileRevalidate } from "workbox-strategies";
+
+cleanupOutdatedCaches();
 
 // @ts-ignore
 precacheAndRoute(self.__WB_MANIFEST, {
@@ -6,3 +12,18 @@ precacheAndRoute(self.__WB_MANIFEST, {
   cleanURLs: true,
   ignoreURLParametersMatching: [/_worker\.js/i],
 });
+
+// dprint-ignore
+registerRoute(({ url }) => url.origin === "https://fonts.googleapis.com", new StaleWhileRevalidate({
+  cacheName: "google-fonts-stylesheets",
+  plugins: [new CacheableResponsePlugin({ statuses: [0, 200] })],
+}));
+
+// dprint-ignore
+registerRoute(({ url }) => url.origin === "https://fonts.gstatic.com", new CacheFirst({
+  cacheName: "google-fonts-webfonts",
+  plugins: [
+    new CacheableResponsePlugin({ statuses: [0, 200] }),
+    new ExpirationPlugin({ maxEntries: 20, maxAgeSeconds: 60 * 60 * 24 * 365 }),
+  ],
+}));
