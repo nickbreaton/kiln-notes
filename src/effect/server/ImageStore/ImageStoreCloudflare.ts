81961dcd731ec2ebe6721b39aba13fe64426425c
Nick Breaton <nick@nickbreaton.com>
2026-02-13 23:14:48 -0500

    Move binding access to method

--- src/effect/server/ImageStore/ImageStoreCloudflare.ts
+++ src/effect/server/ImageStore/ImageStoreCloudflare.ts
@@ -6,13 +6,17 @@ import { ImageStore, ImageStoreError } from "./ImageStore";
 export const ImageStoreCloudflare = Layer.effect(
   ImageStore,
   Effect.gen(function*() {
-    console.log("before");
-    const bindings = yield* Effect.serviceOption(CloudflareBindings);
-    console.log("bindings", bindings);
-    const { IMAGES_BUCKET } = yield* bindings;
-    console.log("IMAGES", IMAGES_BUCKET);
+    const getImagesBucket = Effect.gen(function*() {
+      const bindings = yield* Effect.serviceOption(CloudflareBindings);
+      const { IMAGES_BUCKET } = yield* bindings;
+      return IMAGES_BUCKET;
+    });
 
     const upload = Effect.fn(function*(key: string, file: Stream.Stream<Uint8Array>) {
+      const IMAGES_BUCKET = yield* getImagesBucket.pipe(
+        Effect.catchAllCause(cause => new ImageStoreError({ cause })),
+      );
+
       // @ts-ignore - Effect and Cloudflare opaque type mismatch
       const stream: ReadableStream = Stream.toReadableStream(file);
 
@@ -24,6 +28,10 @@ export const ImageStoreCloudflare = Layer.effect(
 
     const get = (key: string) =>
       Effect.gen(function*() {
+        const IMAGES_BUCKET = yield* getImagesBucket.pipe(
+          Effect.catchAllCause(cause => new ImageStoreError({ cause })),
+        );
+
         const object = yield* Effect.tryPromise({
           try: () => IMAGES_BUCKET.get(key),
           catch: (cause) => new ImageStoreError({ cause }),
